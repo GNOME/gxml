359e69efe20f2f540be1e58688a47a1c9edb05b2
Javier Jardón <jjardon@gnome.org>
2012-08-24 02:34:18 +0900

    autogen.sh: Improve buildsrc != compilationsrc

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,11 @@
 # Run this to generate all the initial makefiles, etc.  You'll want to
 # make sure that you have Makefile.am and configure.ac already.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+OLDDIR=`pwd`
+cd $srcdir
 
 # Set paths if GNOME2_DIR is set
 if [ -n "$GNOME2_DIR" ]; then
@@ -37,6 +40,8 @@ intltoolize --force --copy --automake
 #   srcdir, autoconf, intltool, xml-i18n-toolize (opt), libtool, glib (gettext), automake, aclocal
 #   TODO: test that disabling any of these results in autoreconf and intltoolize handling it
 #         gracefully or re-add checks
+
+cd $OLDDIR
 
 # Run ./configure
 if [ -z "$NOCONFIGURE" ]; then
