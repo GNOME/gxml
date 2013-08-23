edf220543f610e315e3837c5a0ece0c4d63723b3
Richard Schwarting <aquarichy@gmail.com>
2013-08-23 10:18:46 -0400

    examples/js: put generated output into own directory

--- examples/js/document_save_to_path.js
+++ examples/js/document_save_to_path.js
@@ -5,7 +5,7 @@ const Gio = imports.gi.Gio;
 
 function saving_a_document_to_a_path () {
     let doc = GXml.Document.from_path ("bookshelf.xml");
-    doc.save_to_path ("bookshelf2.xml");
+    doc.save_to_path ("output/bookshelf_save_to_path.xml");
 }
 
 saving_a_document_to_a_path ();
