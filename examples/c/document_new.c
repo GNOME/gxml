f808846faac37f8c91f5121b9db6b794276471b7
Daniel Espinosa <esodan@gmail.com>
2017-04-24 19:25:54 -0500

    Porting C examples to new API
    Partial fix for bug:
    
    https://bugzilla.gnome.org/show_bug.cgi?id=773428

--- examples/c/document_new.c
+++ examples/c/document_new.c
@@ -2,49 +2,97 @@
 #include <stdio.h>
 
 int main () {
-  GXmlDocument *doc;
-  GXmlElement *root;
-  GXmlElement *owner;
-  GXmlElement *books;
-  GXmlElement *book;
+  GXmlDomDocument *doc;
+  GXmlDomElement *root;
+  GXmlDomElement *owner;
+  GXmlDomElement *books;
+  GXmlDomElement *book;
+  GError *error = NULL;
   int i;
   char *str;
 
   char *authors[] = { "John Green", "Jane Austen", "J.D. Salinger" };
   char *titles[] = { "The Fault in Our Stars", "Pride & Prejudice", "Nine Stories" };
 
-  doc = gxml_document_new ();
+  doc = (GXmlDomDocument*) gxml_gom_document_new ();
 
   // Add a root node
-  root = gxml_document_create_element (doc, "Bookshelf");
-
-  gxml_node_append_child (GXML_NODE (doc), GXML_NODE (root));
-  // if we tried to add a second one, it would fail :), and a g_warning would be printed
-
-  // Add an owner node
-  owner = gxml_document_create_element (doc, "Owner");
-  gxml_node_append_child (GXML_NODE (root), GXML_NODE (owner));
-  gxml_element_set_attribute (owner, "fullname", "John Green");
-  // TODO: need to figure out what sort of errors these would return,
-  // want the devhelp pages to describe meaningful possible errors
-
-  // Add a collection of books
-  books = gxml_document_create_element (doc, "Books");
-  gxml_node_append_child (GXML_NODE (root), GXML_NODE (books));
-
-  for (i = 0; i < sizeof (authors) / sizeof (char*); i++) {
-    book = gxml_document_create_element (doc, "Book");
-    gxml_element_set_attribute (book, "author", authors[i]);
-    gxml_element_set_attribute (book, "title", titles[i]);
-    gxml_node_append_child (GXML_NODE (books), GXML_NODE (book));
+  root = gxml_dom_document_create_element (doc, "Bookshelf", &error);
+  if (error != NULL) {
+    printf ("Error creating root node: %s", error->message);
+    exit (1);
   }
 
-  // TODO: want a "gxml_node_from_string ()"; make sure document ownership transfer properly
+  gxml_dom_node_append_child (GXML_DOM_NODE (doc), GXML_DOM_NODE (root), &error);
+  if (error != NULL) {
+    printf ("Error appending root to document: %s", error->message);
+    exit (1);
+  }
 
-  str = gxml_node_to_string (GXML_NODE (doc), TRUE, 2);
+  // Add an owner node
+  owner = gxml_dom_document_create_element (doc, "Owner", &error);
+  if (error != NULL) {
+    printf ("Error creating a new element node: %s", error->message);
+    exit (1);
+  }
+  gxml_dom_node_append_child (GXML_DOM_NODE (root), GXML_DOM_NODE (owner), &error);
+  if (error != NULL) {
+    printf ("Error appending child to root: %s", error->message);
+    exit (1);
+  }
+  gxml_dom_element_set_attribute (owner, "fullname", "John Green", &error);
+  if (error != NULL) {
+    printf ("Error setting attribute to Owner element node: %s", error->message);
+    exit (1);
+  }
+
+  // Add a collection of books
+  books = gxml_dom_document_create_element (doc, "Books", &error);
+  if (error != NULL) {
+    printf ("Error creating Books element node: %s", error->message);
+    exit (1);
+  }
+  gxml_dom_node_append_child (GXML_DOM_NODE (root), GXML_DOM_NODE (books), &error);
+  if (error != NULL) {
+    printf ("Error appending Books node to root: %s", error->message);
+    exit (1);
+  }
+
+  for (i = 0; i < sizeof (authors) / sizeof (char*); i++) {
+    book = gxml_dom_document_create_element (doc, "Book", &error);
+    if (error != NULL) {
+      printf ("Error creating %i element node: %s", i, error->message);
+      exit (1);
+    }
+    gxml_dom_element_set_attribute (book, "author", authors[i], &error);
+    if (error != NULL) {
+      printf ("Error setting author attribute to %i element: %s", i, error->message);
+      exit (1);
+    }
+    gxml_dom_element_set_attribute (book, "title", titles[i], &error);
+    if (error != NULL) {
+      printf ("Error setting title attribute to %i eleemnt: %s", i, error->message);
+      exit (1);
+    }
+    gxml_dom_node_append_child (GXML_DOM_NODE (books), GXML_DOM_NODE (book), &error);
+    if (error != NULL) {
+      printf ("Error appending %i child node to Books node: %s", i, error->message);
+      exit (1);
+    }
+    g_object_unref (book);
+  }
+
+  str = gxml_gom_element_write_string (GXML_GOM_ELEMENT (root), &error);
+  if (error != NULL) {
+    printf ("Error writing string from root: %s", error->message);
+    exit (1);
+  }
   printf ("%s:\n%s\n", __FILE__, str);
   g_free (str);
 
+  g_object_unref (owner);
+  g_object_unref (books);
+  g_object_unref (root);
   g_object_unref (doc);
 
   return 0;
